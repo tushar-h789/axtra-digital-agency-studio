8dbf5c9a1ddfb0caf2177afa4ebb0901a02c482e
tushar-h789 <tushar789imran@gmail.com>
2024-12-22 15:46:55 +0600

    project setup done

---
+++ src/app/_components/Cursor.tsx
@@ -0,0 +1,1 @@
+// custom mouse pointer animation
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../app/styles/globals.css";
+import { Kanit } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kanit = Kanit({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-kanit",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${kanit.variable} antialiased`}>{children}</body>
     </html>
   );
 }
