fe675e8b635d8f8ad55cbcb853cc1dc8943c1c02
tushar-h789 <tushar789imran@gmail.com>
2024-12-25 11:56:58 +0600

    footer section design and responsive doen not set animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import BrandsSection from "./_components/Sections/BrandsSection";
 import WhyChooseUsIndex from "./_components/Sections/ChooseUs/WhyChooseUsIndex";
 import CommentsSection from "./_components/Sections/CommentsSection";
 import FeaturedSection from "./_components/Sections/FeaturedSection";
+import Footer from "./_components/Sections/Footer";
 import HeroSection from "./_components/Sections/HeroSection";
 import MarketingAgencySection from "./_components/Sections/MarketingAgencySection";
 import Navbar from "./_components/Sections/Navbar";
@@ -22,6 +23,7 @@ export default function HomePage() {
       <WhyChooseUsIndex />
       <PricingSection />
       <BlogSection />
+      <Footer/>
     </div>
   );
 }
